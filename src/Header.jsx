ede6f14ce69311a17b56e2ee8417d44d65e36c92
dimitriinc <dimitriinc@proton.me>
2024-09-15 14:19:20 -0500

    new mobile nav

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, useRef } from 'react'
-import { Link, NavLink, useLocation } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 
 export default function Header({ activeLink, activateLink }) {
     const [menuOpen, setMenuOpen] = useState(false)
@@ -118,39 +118,57 @@ export default function Header({ activeLink, activateLink }) {
                         >
                             contacto
                         </Link>
-                        
+
                         <div className={`mobile-social-links-container`}>
                             <a
                                 href="https://www.instagram.com/cafevinowinebar/"
                                 target="_blank"
                             >
-                                <img
-                                    src="/images/socials/instagram-logo-regal.png"
-                                    heigth="26px"
-                                    width="26px"
-                                    alt="insta-logo"
-                                />
+                                <svg
+                                    width="25px"
+                                    height="25px"
+                                    viewBox="0 0 24 24"
+                                    fill="none"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    className="mobile-social-link-icon"
+                                >
+                                    <path
+                                        fillRule="evenodd"
+                                        clipRule="evenodd"
+                                        d="M2 6C2 3.79086 3.79086 2 6 2H18C20.2091 2 22 3.79086 22 6V18C22 20.2091 20.2091 22 18 22H6C3.79086 22 2 20.2091 2 18V6ZM6 4C4.89543 4 4 4.89543 4 6V18C4 19.1046 4.89543 20 6 20H18C19.1046 20 20 19.1046 20 18V6C20 4.89543 19.1046 4 18 4H6ZM12 9C10.3431 9 9 10.3431 9 12C9 13.6569 10.3431 15 12 15C13.6569 15 15 13.6569 15 12C15 10.3431 13.6569 9 12 9ZM7 12C7 9.23858 9.23858 7 12 7C14.7614 7 17 9.23858 17 12C17 14.7614 14.7614 17 12 17C9.23858 17 7 14.7614 7 12ZM17.5 8C18.3284 8 19 7.32843 19 6.5C19 5.67157 18.3284 5 17.5 5C16.6716 5 16 5.67157 16 6.5C16 7.32843 16.6716 8 17.5 8Z"
+                                        fill="#160b17"
+                                    />
+                                </svg>
                             </a>
                             <a
                                 href="https://www.facebook.com/CafeYVinoWinebar"
                                 target="_blank"
                             >
-                                <img
-                                    src="/images/socials/facebook_icon_regal.png"
-                                    alt="facebook_logo"
-                                    heigth="26px"
-                                    width="26px"
-                                />
+                                <svg
+                                    width="25px"
+                                    height="25px"
+                                    viewBox="0 0 24 24"
+                                    fill="none"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    className="mobile-social-link-icon"
+                                >
+                                    <path
+                                        fillRule="evenodd"
+                                        clipRule="evenodd"
+                                        d="M2 6C2 3.79086 3.79086 2 6 2H18C20.2091 2 22 3.79086 22 6V18C22 20.2091 20.2091 22 18 22H6C3.79086 22 2 20.2091 2 18V6ZM6 4C4.89543 4 4 4.89543 4 6V18C4 19.1046 4.89543 20 6 20H12V13H11C10.4477 13 10 12.5523 10 12C10 11.4477 10.4477 11 11 11H12V9.5C12 7.567 13.567 6 15.5 6H16.1C16.6523 6 17.1 6.44772 17.1 7C17.1 7.55228 16.6523 8 16.1 8H15.5C14.6716 8 14 8.67157 14 9.5V11H16.1C16.6523 11 17.1 11.4477 17.1 12C17.1 12.5523 16.6523 13 16.1 13H14V20H18C19.1046 20 20 19.1046 20 18V6C20 4.89543 19.1046 4 18 4H6Z"
+                                        fill="#160b17"
+                                    />
+                                </svg>
                             </a>
                             <a
                                 href="https://play.google.com/store/apps/details?id=com.cafeyvinowinebar.Cafe_y_Vino"
                                 target="_blank"
                             >
                                 <img
-                                    src="/images/socials/android-logo-regal.png"
-                                    alt="android_logo"
-                                    heigth="26px"
-                                    width="26px"
+                                    src="/images/socials/android-logo.svg"
+                                    alt="Android logo"
+                                    width="25px"
+                                    height="25px"
                                 />
                             </a>
                         </div>
