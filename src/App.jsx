8421e498c9ad670b2edbd57a2c1c9ad76524ab23
dimitriinc <dimitriinc@proton.me>
2024-09-29 21:30:32 -0500

    admin unbroken

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,32 @@ import Horario from './mainScreens/Horario'
 import Footer from './Footer'
 import Signup from './mainScreens/Signup'
 import { useState } from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import {
+    BrowserRouter as Router,
+    Route,
+    Routes,
+    useLocation,
+} from 'react-router-dom'
 import Carreras from './mainScreens/Carreras'
 import Contacto from './mainScreens/Contacto'
 import Carta from './mainScreens/Carta'
 import Reservas from './mainScreens/Reservas'
 import ScrollToTop from './custom/ScrollToTop'
 
-function MainApp({activateLink, activeLink}) {
+function MainApp({ activateLink, activeLink }) {
+    const location = useLocation()
     return (
         <div>
-            <ScrollToTop />
-            <Header
-                activeLink={activeLink}
-                activateLink={activateLink}
-            />
+            {location.pathname !== '/admin' && (
+                <>
+                    <ScrollToTop />
+                    <Header
+                        activeLink={activeLink}
+                        activateLink={activateLink}
+                    />
+                </>
+            )}
+
             <main>
                 <Routes>
                     <Route
@@ -52,11 +63,25 @@ function MainApp({activateLink, activeLink}) {
                     />
                 </Routes>
             </main>
-            <Footer
-                activeLink={activeLink}
-                activateLink={activateLink}
-            />
+
+            {location.pathname !== '/admin' && (
+                <Footer
+                    activeLink={activeLink}
+                    activateLink={activateLink}
+                />
+            )}
         </div>
+    )
+}
+
+function AdminApp() {
+    return (
+        <Routes>
+            <Route
+                path="/admin"
+                element={<Admin />}
+            ></Route>
+        </Routes>
     )
 }
 
@@ -67,21 +92,11 @@ function App() {
     }
     return (
         <Router>
-            <Routes>
-                <Route
-                    path="/"
-                    element={
-                        <MainApp
-                            activeLink={activeLink}
-                            activateLink={activateLink}
-                        />
-                    }
-                ></Route>
-                <Route
-                    path="/admin"
-                    element={<Admin />}
-                ></Route>
-            </Routes>
+            <MainApp
+                activeLink={activeLink}
+                activateLink={activateLink}
+            />
+            <AdminApp />
         </Router>
     )
 }
